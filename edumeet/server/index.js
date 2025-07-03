4b4253789bde847540d0eef26718e72528c0cc98
Shrishabayari <bayarishrisha@gmail.com>
2025-07-03 17:31:58 +0530

    teacher

--- edumeet/server/index.js
+++ edumeet/server/index.js
@@ -42,7 +42,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin like mobile apps or curl
+    console.log("CORS request origin:", origin); // <--- Add this
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
