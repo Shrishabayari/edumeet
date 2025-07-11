6a7ba489135b187bb9f788df64a2042375ada706
Shrishabayari <bayarishrisha@gmail.com>
2025-07-11 14:56:48 +0530

    added

--- edumeet/client/src/pages/Teacher/SheduleAppointment.js
+++ edumeet/client/src/pages/Teacher/SheduleAppointment.js
@@ -74,7 +74,7 @@ const TeacherSchedule = () => {
     return [
       { 
         day: 'Monday', 
-        slots: ['9:00 AM', '10:00 AM', '11:00 AM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM'] 
+        slots: ['9:00 AM 10:00AM', '10:00 AM', '11:00 AM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM'] 
       },
       { 
         day: 'Tuesday', 
